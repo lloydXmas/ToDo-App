fdcbdc7baca318a2afd73e7b4eaefb80f545ead8
Noel Honeybourne <noel@honeybourne.net>
2018-05-27 09:36:24 +0000

    New/remove entries

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ToDo-App
-A basic ToDo app built with Node and PostrgeSQL
+A basic To-Do app built with Node using ExpressJS, Nunjucks, and PostrgeSQL
+
+### Prerequisites
+- Node.js v8.x
+- PostgreSQL running with a database named `todo`
+ 
+### Installing
+```
+npm install express nunjucks pg-promise body-parser
+```
+Create the `tasks` table:                                                                                 
+```
+CREATE TABLE task (
+  id serial PRIMARY KEY,
+  title varchar,
+  description varchar,
+  done boolean DEFAULT FALSE
+);
+```
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const body_parser = require('body-parser');
 const pgp = require('pg-promise')({});
-const db = pgp({database: 'todo', user: 'postgres'});
+const db = pgp({
+  database: 'todo',
+  user: 'postgres'
+});
 
 var app = express();
 
@@ -11,50 +14,55 @@ nunjucks.configure('views', {
   express: app,
   noCache: true
 });
-app.use(body_parser.urlencoded({extended: false}));
-app.use(body_parser.json());
+app.use(body_parser.urlencoded({
+  extended: false
+}));
 app.use(express.static('public'));
 
-app.get('/todos', function (request, response, next) {
-  db.any('SELECT * FROM task')
-   .then(function (results) {
-     response.send(results);
-   })
-   .catch(next);
-   response.render('todos.html');
+app.get('/todos', function(request, response, next) {
+  const mylist = [];
+  db.any('SELECT * FROM task WHERE done = FALSE')
+    .then(function(results) {
+      results.forEach(function(r) {
+        mylist.push(r);
+      });
+      response.render('todos.html', {
+        title: 'All Entries',
+        results: mylist
+      });
+    })
+    .catch(next);
 });
 
- app.get('/todos/add', function (request, response, next) {
-   response.render('new.html');
- });
+app.get('/todos/add', function(request, response, next) {
+  response.render('new.html', {title: 'New Entry'});
+});
 
-app.post('/submit', function (request, response, next) {
+app.post('/submit', function(request, response, next) {
   var attr = {
     title: request.body.title,
     description: request.body.description
   };
   var q = "INSERT INTO task VALUES(default, ${title}, ${description})"
   db.one(q, attr)
-  .then(function (result) {
-    console.log(result);
-  })
-  .catch(next);
+    .then(function(result) {
+      console.log(result);
+    })
+    .catch(next);
   response.redirect('/todos');
 });
 
-app.get('/todo/done/:id', function (request, response, next) {
-  var slug = request.params.slug;
-  var q = "UPDATE task SET done = 'FALSE' WHERE id = $1, [slug]";
-  db.result(q)
-  .then(function (result) {
-    console.log(result);
-  })
-  .catch(next);
-
-  response.redirect('/todos.html');
-
+app.get('/todo/done/:id', function(request, response, next) {
+  var id = request.params.id;
+  var q = "UPDATE task SET done = 'TRUE' WHERE id = $1";
+  db.one(q, [id])
+    .then(function(result) {
+      console.log(result);
+    })
+    .catch(next);
+  response.redirect('/todos');
 });
 
-app.listen(8080, function () {
+app.listen(8080, function() {
   console.log('Listening on port 8080');
 });
